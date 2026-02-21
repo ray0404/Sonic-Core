d14c4c62512d9ff269685a80edefe51fcadb924e
ray0404 <rayvalentinmusic@gmail.com>
2026-02-21 09:40:33 -0700

    fix(core): correct relative import path for logger in worklets

--- packages/sonic-core/src/worklets/DynamicEQNode.ts
+++ packages/sonic-core/src/worklets/DynamicEQNode.ts
@@ -1,4 +1,4 @@
-import { logger } from "../../utils/logger.js";
+import { logger } from "../utils/logger.js";
 import { AudioWorkletNode, IAudioContext, IOfflineAudioContext, TAudioWorkletNodeConstructor } from "standardized-audio-context";
 
 const AudioWorkletNodeBase = AudioWorkletNode as TAudioWorkletNodeConstructor;
